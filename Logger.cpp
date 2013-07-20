68ecf7c23ce2a07def64eed8138bf7cb69f3cdb1
Antonis Kalou <kalouantonis@gmail.com>
2013-07-20 17:53:10 +0000

    Implemented logger

---
+++ Logger.cpp
@@ -0,0 +1,48 @@
+#include "Logger.h"
+
+// For time logging
+#include <ctime>
+
+// For int to string conversion
+#include <sstream>
+
+namespace SuperEngine
+{
+    bool Logger::Init(const std::string& filename)
+    {
+        // Logger won't append, i dont know, might implement this later
+        m_out.open(filename.c_str(), std::ios::out);
+
+        //m_writeTime = true;
+
+        return m_out.is_open();
+    }
+
+    void Logger::writeTime()
+    {
+        if(m_writeTime)
+        {
+            time_t now = time(0);
+
+            char timeStr[80];
+            strftime(timeStr, 80, "%d-%b-%Y %R:%S", localtime(&now));
+
+            m_out << timeStr << " : ";
+            m_writeTime = false;
+        }
+    }
+
+    void Logger::flush()
+    {
+        m_writeTime = true;
+        m_out.flush();
+    }
+
+    Logger& operator<<(Logger& os, std::ostream& (*fun)(std::ostream&))
+    {
+        os.setWriteTime(true);
+
+        os.stream() << fun;
+        return os;
+    }
+};
---
+++ Logger.h
@@ -0,0 +1,53 @@
+#ifndef _LOGGER_H_
+#define _LOGGER_H_
+
+#include <fstream>
+#include <ios>
+
+#define ERR "[!!] "
+#define WARN "[W] "
+#define INFO "[#] "
+
+#include <tr1/memory>
+
+namespace SuperEngine
+{
+
+    class Logger
+    {
+    private:
+        std::ofstream m_out;
+
+        bool m_writeTime;
+
+    public:
+        Logger() { }
+        ~Logger() { }
+        bool Init(const std::string& filename);
+
+        std::ostream& stream() { return m_out; }
+
+        void writeTime();
+
+        bool getWriteTime() { return m_writeTime; }
+        void setWriteTime(bool val) { m_writeTime = val; }
+
+        void flush();
+
+    };
+
+    template<typename T>
+    Logger& operator<<(Logger& os, T val)
+    {
+        os.writeTime();
+        os.stream() << val;
+        return os;
+    }
+
+    // Use function pointer that uses ostream references as parameters and returns
+    // and execute it. So anything like std::endl will work.
+    Logger& operator<<(Logger& os, std::ostream& (*fun)(std::ostream&));
+};
+
+
+#endif // _LOGGER_H_
