0e3803c122d17d8cfdda4af7e730e1163382874d
Antonis Kalou <kalouantonis@gmail.com>
2013-09-02 17:06:06 +0300

    Collision detection in groups

--- src/Graphics/DrawableGroup.cpp
+++ src/Graphics/DrawableGroup.cpp
@@ -1,4 +1,5 @@
 #include <Engine.h>
+#include <math.h>
 
 namespace SuperEngine
 {
@@ -47,5 +48,44 @@ namespace SuperEngine
     {
         for(auto it = begin(); it != end(); ++it)
             (*it)->Update(elapsedTime);
+    }
+
+    DrawableGroup drawablecollide(Drawable* drawable, DrawableGroup* group,
+                                           bool kill, bool(*collided)(Drawable*, Drawable*))
+    {
+        DrawableGroup collidedDrawables;
+
+        for(auto it = group->begin(); it != group->end(); ++it)
+        {
+            if(collided != 0)
+            {
+                if(collided(drawable, (*it)))
+                {
+                    collidedDrawables.push_back(*it);
+                }
+            }
+            else if(drawable->getCollisionMethod() != COLLISION_NONE &&
+                    (*it)->getCollisionMethod() != COLLISION_NONE)
+            {
+                if(drawable->getCollisionMethod() == COLLISION_RECT)
+                {
+                    if((abs(drawable->getX() - (*it)->getX()) * 2 < (drawable->getSize().x + (*it)->getSize().x)) &&
+                       (abs(drawable->getY() - (*it)->getY()) * 2 < (drawable->getSize().y + (*it)->getSize().y)))
+                    {
+                        // Bounding box collision has occured
+                        collidedDrawables.push_back(*it);
+
+                        if(kill)
+                            group->remove(it);
+                    }
+                }
+                else if(drawable->getCollisionMethod() == COLLISION_DIST)
+                {
+                    // distance based collision
+                }
+            }
+        }
+
+        return collidedDrawables;
     }
 };
